8a7bb67f0f28552cb6070402d872510f04843f3f
CUYORITO <silvafragosomisael@gmail.com>
2023-09-18 01:36:54 -0600

    Actualizacion

--- src/Archivos playlist/Clase_13_08_2023.java
+++ src/Archivos playlist/Clase_13_08_2023.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-
+import java.util.Collections;
 import javax.swing.JOptionPane;
 
 public class Clase_13_08_2023{
@@ -33,8 +33,30 @@ public class Clase_13_08_2023{
     public String ordenado (){
         String aux = "";
         for(int i = 0; i < arrays.size() ; i++){
-            aux += (((i+1) + ")") + (arrays.get(i).get(0)) + "\n");
+            aux += ((i+1) + (arrays.get(i).get(0)) + "\n");
         }
+        return aux;
+    }
+
+    public String ordenadoCanciones(){
+        String aux = "";
+        int numeros = 1;
+        Collections.sort(arrays.get(posicion(playlist)));
+
+        System.out.println(arrays);
+
+        for(int i = 1; true ; i++){
+            try{
+                if((arrays.get(posicion(playlist)).get(i)).equals(playlist)){
+                }else{
+                    aux += ((numeros + ") ") + (arrays.get(posicion(playlist)).get(i)) + "\n");
+                    numeros++;
+                }
+            }catch(Exception e){
+
+                break;
+            }
+        }        
         return aux;
     }
 
@@ -54,11 +76,15 @@ public class Clase_13_08_2023{
         if(arrays.isEmpty()){
             cventana("No tienes playlist.");
         }else{
-            playlist = crear("Elige tu playlist \n" + ordenado());    
-            if(buscar(playlist)){
-                ventanaMenu();
-            }else{
-                cventana("No existe la playlist");
+            try{
+                playlist = ") " + (crear("Elige tu playlist \n" + ordenado()));    
+                if(buscar(playlist)){
+                    menuCanciones(ventanaMenu());
+                }else{
+                    cventana("No existe la playlist");
+                }
+            }catch (Exception e){
+                    menuPlaylist(ventanaPlaylist());    
             }
         }
     }
@@ -79,7 +105,7 @@ public class Clase_13_08_2023{
     public void crearPlaylist (){
 
         ArrayList <String> interno = new ArrayList<>();
-        interno.add(crear("Nombre de playlis:"));
+        interno.add(") " + crear("Nombre de playlis:"));
         arrays.add(interno);
     }
 
@@ -105,9 +131,9 @@ public class Clase_13_08_2023{
                 if(buscar(list)){
                     
                     arrays.remove(posicion(list));
-                    cventana("Se ha borrado tu cancion");
+                    cventana("Se ha borrado tu playlist");
                 }else{
-                    cventana("La cancion no existe");
+                    cventana("La playlist no existe");
                 }
             }
         }
@@ -139,24 +165,107 @@ public class Clase_13_08_2023{
         "\n 3) Mostrar canciones" +
         "\n 4) Eliminar cancion " +
         "\n 5) Limpiar playlis " + 
-        "\n 6) Atras",
+        "\n 6) Atras" + 
+        "\n 7) Salir",
         "Menu de playlist",
         1)).toLowerCase();
     }
 
-    public void agregar(){
+    public void agregarCancion(){
         int aumentado = 1;
         arrays.get(posicion(playlist)).add(aumentado, "nulo");
         arrays.get(posicion(playlist)).set(aumentado, crear("¿Que cancion guardaras?"));
         aumentado++;
+        menuCanciones(ventanaMenu());
+        
     }
 
+    public void modificarCancion() {
+        try{
+            if(arrays.get(posicion(playlist)).get(1).isEmpty()){
+            }else{
+                int cancion = (Integer.parseInt(crear("¿Que cancion deseas modificar? \n" + ordenadoCanciones())));
+                arrays.get(posicion(playlist)).set(cancion, crear("Escribe tu modificacion:"));
+                menuCanciones(ventanaMenu());
+            }
+        }catch (Exception e){
+            cventana("Playlist vacia");
+            menuCanciones(ventanaMenu());
+        }
+    }
+
+    public void mostrarCanciones(){
+        
+        try{
+            if(arrays.get(posicion(playlist)).get(1).isEmpty()){
+            }else{
+                cventana("Las canciones de tu playlist \"" + (playlist.replace("- ", "")) + "\" son: \n" + ordenadoCanciones());
+                menuCanciones(ventanaMenu());
+            }
+        }catch (Exception e){
+            cventana("Playlist vacia");
+            menuCanciones(ventanaMenu());
+        }
+    }
+
+    public void eliminarCanciones(){
+
+        try{
+            if(arrays.get(posicion(playlist)).get(1).isEmpty()){
+            }else{
+                try{
+                    int cancion = (Integer.parseInt(crear("¿Que cancion deseas eliminar? \n" + ordenadoCanciones())));
+                    arrays.get(posicion(playlist)).remove(cancion);
+                    cventana("Se ah eliminado tu cancion");
+                }catch (Exception e){
+                    cventana("No existe la cancion");
+                }
+                menuCanciones(ventanaMenu());
+            }
+        }catch (Exception e ){
+            cventana("Playlist vacia");
+            menuCanciones(ventanaMenu());
+        }
+    }
+
+    public void limpiarCanciones(){  
+
+        for(int i = 1; i < (arrays.get(posicion(playlist)).size()) ; i++){
+            arrays.get(posicion(playlist)).remove(i);
+        }
+        cventana("Playlist vacia");
+        menuCanciones(ventanaMenu());
+    }
+
+    public void atras(){
+        menuPlaylist(ventanaPlaylist());
+    }
+
+    public void menuCanciones (String valor){
+        if(valor.equals("1") || valor.equals("agregar canciones")){
+            agregarCancion();
+        }else if(valor.equals("2") || valor.equals("modificar canciones")){
+                modificarCancion();
+        }else if (valor.equals("3") || valor.equals("mostrar canciones")){
+            mostrarCanciones();
+        }else if(valor.equals("4") || valor.equals("eliminar cancion")){
+            eliminarCanciones();
+        }else if(valor.equals("5") || valor.equals("limpiar playlist")){
+            limpiarCanciones();
+        }else if(valor.equals("6") || valor.equals("atras")){
+            atras();
+        }else if(valor.equals("7") || valor.equals("salir")){
+            System.exit(0);
+        }else{
+            JOptionPane.showMessageDialog(null, "Mamo, Escribe bien");
+        }
+    }
     public static void main(String[] args) {
 
         Clase_13_08_2023 clase = new Clase_13_08_2023();
-        
-        
-        
-    }
 
+        do{
+            clase.menuPlaylist(clase.ventanaPlaylist());
+        }while(true);
+    }
 }
